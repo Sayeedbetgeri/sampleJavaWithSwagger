1af55cd29164a5a691bcdbffa76dd7aeafd55778
Sayeedahmed A Betgeri <Sayeedbetgeri@gmail.com>
2021-07-16 20:14:53 +0530

    Add files via upload

---
+++ src/main/java/openshift/test/openshiftjava/OpenshiftJavaApplication.java
@@ -0,0 +1,13 @@
+package openshift.test.openshiftjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftJavaApplication.class, args);
+	}
+
+}
